028a3823226e7897111b9ef0a7678347ee1efc98
wxx2014544064 <1208354917@qq.com>
2019-02-02 12:06:32 +0800

    Initial commit

---
+++ JavaWeb01/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: wxx
+  Date: 2019/2/1
+  Time: 23:02
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>传智播客官网</title>
+</head>
+<body>
+<h1 style="color: blue">Hello   ITCAST</h1>
+</body>
+</html>
---
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,18 @@
+package com.atguigu.bean;
+
+
+public class Customer {
+    //这是一个Java程序
+    public static void main(String[] args){
+
+        System.out.println("hello     mvc");
+
+        System.out.println("hello     mvc");
+        System.out.println("hello     mvc");
+
+
+        System.out.println("hello     mvc");
+        System.out.println("hello     mvc");
+        System.out.println("hello     mvc");
+    }
+}
